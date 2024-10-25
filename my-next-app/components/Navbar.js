3c044b74038d815ec997b9acf16d5cd93fba2b02
John Doe <test@example.org>
2024-10-25 10:05:22 +0300

    Initial commit

---
+++ my-next-app/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() { return (
+    <nav>
+        <a href='/' >навбар</a>
+        <a href='/about' >нет нет нет</a>
+        <a href='/contact' >да да да</a>
+    </nav>
+); }
---
+++ my-next-app/pages/Index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Добро пожаловать на мой сайт Максим Ринатович :p</h1>
+        <p>Чета чета чета чета чета чета чета чета.</p>
+      </div>
+    );
+  }
